aebb3f5a2f7d87665b3cd7234f6cafaded490927
Nolan Docherty <sodinokazaki@gmail.com>
2021-08-30 21:01:30 +0100

    add cross and circle for the board

---
+++ src/components/board/components/circle/circle.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './circle.css';
+export const Circle = () =>  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" >
+    <circle cx="50" cy="50" r="30"  className="circle"/>
+</svg>
